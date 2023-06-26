69a14a384c0ba9dae31ec5b4b906b261bfa2eb57
Christina Mari <christinario365@gmail.com>
2023-06-26 11:40:45 -0700

    Initial Commit

---
+++ track4me/track4meApp.swift
@@ -0,0 +1,17 @@
+//
+//  track4meApp.swift
+//  track4me
+//
+//  Created by Christina Rioflorido on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct track4meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
