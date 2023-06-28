a5d7d454ae419b522c74922844237609f37766f0
Christina Mari <christinario365@gmail.com>
2023-06-28 13:13:21 -0700

    m

---
+++ track4me/moodTracker.swift
@@ -0,0 +1,20 @@
+//
+//  moodTracker.swift
+//  track4me
+//
+//  Created by Christina Rioflorido on 6/28/23.
+//
+
+import SwiftUI
+
+struct moodTracker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct moodTracker_Previews: PreviewProvider {
+    static var previews: some View {
+        moodTracker()
+    }
+}
---
+++ track4me/toDoList.swift
@@ -0,0 +1,20 @@
+//
+//  toDoList.swift
+//  track4me
+//
+//  Created by Christina Rioflorido on 6/28/23.
+//
+
+import SwiftUI
+
+struct toDoList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct toDoList_Previews: PreviewProvider {
+    static var previews: some View {
+        toDoList()
+    }
+}
