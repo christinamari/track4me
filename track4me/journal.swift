16c6619342f4c4acd16a6408f4eb77db9edd04ba
Christina Mari <christinario365@gmail.com>
2023-06-29 10:45:42 -0700

    journalll

--- track4me/journal.swift
+++ track4me/journal.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct journal: View {
+    @State private var thoughts = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ZStack {
+                Image("journalBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .opacity(1.0)
+                ZStack {
+                    Image("journalOverlay")
+                        .resizable()
+                    Text("Journal")
+                        .font(.custom("Chewy-Regular", size: 70))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 575)
+                    Text("Let it out!")
+                        .font(.custom("Chewy-Regular", size: 35))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 470)
+                }
+                TextField("Write down your thoughts here...", text: $thoughts)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.black)
+                    .background(.white)
+            }
+        }
     }
 }
 
