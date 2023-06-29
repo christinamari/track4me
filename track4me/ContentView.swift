d6f03487b6ff8aa7a123922c716f9b3624308cc4
Christina Mari <christinario365@gmail.com>
2023-06-29 00:05:12 -0700

    m

--- track4me/ContentView.swift
+++ track4me/ContentView.swift
@@ -9,39 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.cyan
+        GeometryReader { geo in
             NavigationStack {
-                Text("track4me")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                VStack {
-                    // journal
-                    NavigationLink(destination: journal()) {
-                        Text("Journal")
-                    }
-                    .padding()
-                    // to do list
-                    NavigationLink(destination: toDoList()) {
-                        Text("To Do List")
-                    }
-                    .padding()
-                    // sleep tracker
-                    NavigationLink(destination: sleepTracker()) {
-                        Text("Sleep Tracker")
-                    }
-                    .padding()
-                    // mood tracker
-                    NavigationLink(destination: moodTracker()) {
-                        Text("Mood Tracker")
-                    }
-                    .padding()
+                ZStack {
+                    Image("background1")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                        .opacity(1.0)
+                    
+                    VStack {
+                        Text("track4me")
+                            .font(.custom("Chewy-Regular", size: 50))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        // journal
+                        NavigationLink(destination: journal()) {
+                            Text("Journal")
+                                .font(.custom("Kollektif", size: 30))
+                                .foregroundStyle(.black)
                         }
+                        .padding()
+                        // to do list
+                        NavigationLink(destination: toDoList()) {
+                            Text("To Do List")
+                            .font(.custom("Kollektif", size: 30))
+                            .foregroundStyle(.black)
+                        }
+                        .padding()
+                        // sleep tracker
+                        NavigationLink(destination: sleepTracker()) {
+                            Text("Sleep Tracker")
+                            .font(.custom("Kollektif", size: 30))
+                            .foregroundStyle(.black)
+                        }
+                        .padding()
+                        // mood tracker
+                        NavigationLink(destination: moodTracker()) {
+                            Text("Mood Tracker")
+                            .font(.custom("Kollektif", size: 30))
+                            .foregroundStyle(.black)
+                        }
+                        .padding()
                     }
-        }
+                    
+                }
             }
         }
+    }
+}
+
+/*
+VStack {
+    NavigationStack {
+        Text("track4me")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .multilineTextAlignment(.center)
+            // journal
+            NavigationLink(destination: journal()) {
+                Text("Journal")
+            }
+            .padding()
+            // to do list
+            NavigationLink(destination: toDoList()) {
+                Text("To Do List")
+            }
+            .padding()
+            // sleep tracker
+            NavigationLink(destination: sleepTracker()) {
+                Text("Sleep Tracker")
+            }
+            .padding()
+            // mood tracker
+            NavigationLink(destination: moodTracker()) {
+                Text("Mood Tracker")
+            }
+            .padding()
+        }
+    }
+*/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- track4me/track4meApp.swift
+++ track4me/track4meApp.swift
@@ -15,3 +15,4 @@ struct track4meApp: App {
         }
     }
 }
+
